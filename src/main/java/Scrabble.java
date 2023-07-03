5fed04ea77e2f675039da078fe52e2e3a5934aba
kenichi beveridge <kenbev84@gmail.com>
2023-07-03 12:08:27 +0100

    Initial setup and first test passed

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,2 +1,12 @@
 public class Scrabble {
+
+    private String word;
+    public Scrabble(String userWord) {
+        this.word = userWord;
+    }
+
+    public int score() {
+        return 0;
+    }
+
 }
--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -2,6 +2,7 @@
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScrabbleTest {
